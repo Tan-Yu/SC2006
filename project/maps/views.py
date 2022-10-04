6ea3be6c8fa7fa682403bbe87e8e0909a8cbe846
FreedomXV <94072359+FreedomXV@users.noreply.github.com>
2022-10-04 22:54:09 +0800

    created map app, added requirements.txt

---
+++ project/maps/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def default_map(request):
+    return render(request, 'default.html', {})
--- project/project/urls.py
+++ project/project/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("hawkerapp/", include("hawkerapp.urls"))
+    path("hawkerapp/", include("hawkerapp.urls")),
+    path('maps/', include('maps.urls')),
 ]
