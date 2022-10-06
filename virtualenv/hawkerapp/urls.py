8eb1d9257f90d6a6effd12d283ae1597fda96ca7
Tan-Yu <tanyuweb@gmail.com>
2022-10-06 16:35:17 +0800

    Create Virtual Environment virtualenv

---
+++ virtualenv/hawkerapp/urls.py
@@ -0,0 +1,15 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("registercustomer/", views.registercustomer, name="registercustomer"),
+    path("registerhawker/", views.registerhawker, name="registerhawker"),
+    path("login/", views.loginPage, name="login"),
+    path("logout/", views.logoutUser, name="logout"),
+    path("stallprofilehawker/<int:stall_id>", views.stallprofilehawker, name="stallprofilehawker"),
+    path("stallprofilecustomer/<int:stall_id>", views.stallprofilecustomer, name="stallprofilecustomer"),
+    path("hawkerprofile/", views.hawkerprofile, name="hawkerprofile"),
+    path("addstall/", views.addstall, name="addstall"),
+    path("addreview/<int:stall_id>", views.addreview, name="addreview"),
+]
