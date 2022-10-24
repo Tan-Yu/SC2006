9df7873056100eb8fdcbfd006fbf35f8c8740306
Foo Jin Rui <foojinrui123@gmail.com>
2022-10-24 19:24:16 +0800

    added many things

--- project/hawkerapp/admin.py
+++ project/hawkerapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Food, Location, Stall, Review
+from .models import Location, Stall, Review, Menu
 # Register your models here.
-admin.site.register(Food)
 admin.site.register(Location)
 admin.site.register(Stall)
 admin.site.register(Review)
+admin.site.register(Menu)
--- project/project/settings.py
+++ project/project/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "mathfilters",
     "hawkerapp",
     "django.contrib.admin",
     "django.contrib.auth",
