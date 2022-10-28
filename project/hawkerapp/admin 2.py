1fdfc2ec6e21d07e19a40c0f94bd6dd9eb7a5fba
Tan-Yu <tanyuweb@gmail.com>
2022-10-28 21:46:51 +0800

    Remove Duplicates

--- project/hawkerapp/admin 2.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Location, Stall, Review, Menu
-# Register your models here.
-admin.site.register(Location)
-admin.site.register(Stall)
-admin.site.register(Review)
-admin.site.register(Menu)
