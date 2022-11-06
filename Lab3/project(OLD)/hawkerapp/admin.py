61386b2c1407d705dd21489c673fc1d121ded3ea
Tan-Yu <tanyuweb@gmail.com>
2022-11-06 11:05:42 +0800

    Application Skeleton

--- Lab3/project(OLD)/hawkerapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Food, Location, Stall, Review
-# Register your models here.
-admin.site.register(Food)
-admin.site.register(Location)
-admin.site.register(Stall)
-admin.site.register(Review)
--- Lab3/project(OLD)/maps/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [ 
-    path('',views.maps, name = 'coordinates-form'),
-    # path('map', views.maps, name = 'maps'),
-]
